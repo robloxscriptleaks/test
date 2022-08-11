950ea9431126c59bb3c54a6469e14789f4e20384
robloxscriptleaks <98795870+robloxscriptleaks@users.noreply.github.com>
2022-08-11 14:41:49 -0400

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,18 @@
+local plr = game:GetService("Workspace")
+local target = plr["alsolaggin"]
+local targett = target.Name
+if target then
+local a = Instance.new("BillboardGui",target)
+a.Size = UDim2.new(3,0, 3,0)
+a.Name = "A"
+a.AlwaysOnTop = true
+local b = Instance.new("Frame",a)
+   b.Size = UDim2.new(1,0, 1,0)
+   b.BackgroundTransparency = 0.4
+b.BorderSizePixel = 0
+local c = Instance.new("TextLabel",b)
+c.Text = targett
+c.Size = UDim2.new(1,0, 1,0)
+c.BackgroundTransparency = 1
+c.BorderSizePixel = 0
+end
